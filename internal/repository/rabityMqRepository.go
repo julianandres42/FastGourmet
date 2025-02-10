9a69d9d794d3efa214802f546c586889de67f295
Julian andres Duque angulo <julianandresduqueangulo@Julians-MacBook-Pro.local>
2025-02-09 21:17:02 -0500

    add hexagonal service

--- internal/repository/rabityMqRepository.go
+++ internal/repository/rabityMqRepository.go
@@ -2,15 +2,15 @@ package repository
 
 import "FastGourmet/internal/core/domain"
 
-type RabityMqRepository struct {
+type RabittMqRepository struct {
 }
 
-func (r RabityMqRepository) Enqueue(order domain.Order) error {
+func (r RabittMqRepository) Enqueue(order domain.Order) error {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (r RabityMqRepository) Receive(order domain.Order) error {
+func (r RabittMqRepository) Receive(order domain.Order) error {
 	//TODO implement me
 	panic("implement me")
 }
